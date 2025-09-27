810c4afa3cf4159a9ec12302b3040972cc5d5595
Vineeth JR <146830394+vineethjr@users.noreply.github.com>
2025-09-27 17:29:55 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+const buttons = document.querySelectorAll('.product-card button');
+
+buttons.forEach(button => {
+  button.addEventListener('click', () => {
+    alert('Product added to cart!');
+  });
+});
